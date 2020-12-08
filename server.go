f6a380fa86caf876cb82ef191ab87546163f8d32
murali-crish <murali.crish@gmail.com>
2020-12-08 13:04:02 +1000

    Before Integration Tests
    Added Inmemory store to store Player scores.
    But that is not working solution. That only passes tests. We need more working software.

--- server.go
+++ server.go
@@ -8,20 +8,27 @@ import (
 
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
 type PlayerServer struct {
 	store PlayerStore
 }
 
+func (i *InMemoryPlayerStore) RecordWin(name string) {}
+
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
-	}
-
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
@@ -29,4 +36,9 @@ func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
 }
